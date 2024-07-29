4b34e1c9e25720ae564102f83d364dad5ba533fd
J3Destroyer <mrj3minecraft@gmail.com>
2024-07-29 12:29:10 +0800

    Small update to SettingsActivity

--- app/src/main/java/com/singhealth/enhance/activities/settings/SettingsActivity.kt
+++ app/src/main/java/com/singhealth/enhance/activities/settings/SettingsActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.Firebase
