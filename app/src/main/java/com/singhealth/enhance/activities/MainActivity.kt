ac6270b4287b97d674b99d1b24283396286e7515
carljdevan <carljdevan@gmail.com>
2024-10-08 23:15:39 +0800

    7DayAvgCalc Final

--- app/src/main/java/com/singhealth/enhance/activities/MainActivity.kt
+++ app/src/main/java/com/singhealth/enhance/activities/MainActivity.kt
@@ -23,7 +23,6 @@ import com.singhealth.enhance.security.SecureSharedPreferences
 import com.singhealth.enhance.security.StaffSharedPreferences
 
 class MainActivity : AppCompatActivity() {
-class MainActivity : BaseActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
